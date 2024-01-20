ef0739e86f13a9ab71bdd124a43e4127260232b2
jasonoh22 <jasonoh22@naver.com>
2024-01-20 19:02:09 +0900

    refactor: 시크릿 키 가져오기 변경
    시크릿 키 가져오기 변경

--- src/main/java/com/ll/demo/member/controller/MemberController.java
+++ src/main/java/com/ll/demo/member/controller/MemberController.java
@@ -49,8 +49,9 @@ public class MemberController {
                         "username", member.getUsername(),
                         "authorities", member.getAuthoritiesAsStrList()
                 )
-                , jwtProperties.getSECRET_KEY()
+                , jwtProperties.getSecretKey()
         );
+        System.out.println("accessToken 통과");
         // refreshToken 생성 accessToken의 유효 시간이 만료되었을 때 새로운 accessToken 발급 받기 위해 사용
         String refreshToken = JwtUtil.encode(
                 60 * 60 * 24, //1 day
@@ -58,7 +59,7 @@ public class MemberController {
                         "id", member.getId().toString(),
                         "username", member.getUsername()
                 )
-                , jwtProperties.getSECRET_KEY() // JWT를 생성할 때 사용하는 비밀키
+                , jwtProperties.getSecretKey() // JWT를 생성할 때 사용하는 비밀키
         );
         memberService.setRefreshToken(member, refreshToken);
 
@@ -90,7 +91,7 @@ public class MemberController {
                         "username", member.getUsername(),
                         "authorities", member.getAuthoritiesAsStrList()
                 ),
-                jwtProperties.getSECRET_KEY()
+                jwtProperties.getSecretKey()
         );
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", accessToken)
                 .path("/")
