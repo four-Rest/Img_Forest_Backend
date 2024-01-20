b536b5d415a54c70d84e1fa1c85e55015a26f6a5
jasonoh22 <jasonoh22@naver.com>
2024-01-20 19:47:43 +0900

    refactor: getSecretKey 수정
    getSecretKey 수정

--- src/main/java/com/ll/demo/member/controller/MemberController.java
+++ src/main/java/com/ll/demo/member/controller/MemberController.java
@@ -49,7 +49,7 @@ public class MemberController {
                         "username", member.getUsername(),
                         "authorities", member.getAuthoritiesAsStrList()
                 )
-                , jwtProperties.getSECRET_KEY()
+                , jwtProperties.getSecretKey()
         );
         System.out.println("accessToken 통과");
         // refreshToken 생성 accessToken의 유효 시간이 만료되었을 때 새로운 accessToken 발급 받기 위해 사용
@@ -59,7 +59,7 @@ public class MemberController {
                         "id", member.getId().toString(),
                         "username", member.getUsername()
                 )
-                , jwtProperties.getSECRET_KEY() // JWT를 생성할 때 사용하는 비밀키
+                , jwtProperties.getSecretKey() // JWT를 생성할 때 사용하는 비밀키
         );
         memberService.setRefreshToken(member, refreshToken);
 
@@ -92,7 +92,7 @@ public class MemberController {
                         "username", member.getUsername(),
                         "authorities", member.getAuthoritiesAsStrList()
                 ),
-                jwtProperties.getSECRET_KEY()
+                jwtProperties.getSecretKey()
         );
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", accessToken)
                 .path("/")
