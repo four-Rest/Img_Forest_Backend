9ceefdbb97cbb1b03020c7dc2463bac9609f0c68
jasonoh22 <jasonoh22@naver.com>
2024-01-20 19:00:14 +0900

    refactor: JWT 관련 작업
    JWT 관련 작업

--- src/main/java/com/ll/demo/global/config/JwtProperties.java
+++ src/main/java/com/ll/demo/global/config/JwtProperties.java
@@ -1,6 +1,5 @@
 package com.ll.demo.global.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,7 +9,8 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties("jwt")
+@ConfigurationProperties("spring.jwt")
 public class JwtProperties {
-    private String SECRET_KEY;
+    private String secretKey;
 }
+
