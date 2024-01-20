f96b6b8deb6c92ec76b861ffaa7c83d79389163c
tallguy188 <cksghd4220@naver.com>
2024-01-20 15:28:15 +0900

    refactor: Security 관련 수정
    Security 관련 수정

--- src/main/java/com/ll/demo/global/config/SecurityConfig.java
+++ src/main/java/com/ll/demo/global/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
