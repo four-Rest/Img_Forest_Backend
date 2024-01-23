9c9ba07d680446ea4287e48701fdecca96082bd2
tallguy188 <cksghd4220@naver.com>
2024-01-23 17:22:35 +0900

    fix: h2-console 차단 관련 ApiSecurityConfig 수정 ApiSecurityConfig에 /h2-console/** permit all 설정

--- src/main/java/com/ll/demo/global/config/ApiSecurityConfig.java
+++ src/main/java/com/ll/demo/global/config/ApiSecurityConfig.java
@@ -19,6 +19,8 @@ public class ApiSecurityConfig {
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/**")
+                .authorizeHttpRequests(authorizeRequests ->authorizeRequests
+                        .requestMatchers("/h2-console/").permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         ;
