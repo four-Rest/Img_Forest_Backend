d2a188c3acfafd69debb83fdc55e8a6082b4a2f8
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2024-01-29 16:01:06 +0900

    dev -> client (#32)
    * feat: 파일생성 시 mac 버전 추가
    
    * refactor: 게시물 전체 조회 Dto 변경
    
    * feat: tag검색 구현
    
    * style: AticleController에서 메서드명을 직관적으로 변경
    
    * feat: ArticleTag엔티티 생성, 관련 로직 생성
    
    article과 tag의 다대다 맵핑 구현을 위해 중간 엔티티인 ArticleTag 생성,
    이를 통한 tag 검색 구현
    
    * fix: conflict
    
    ---------
    
    Co-authored-by: Bang210 <bangvelope@gmail.com>
    Co-authored-by: Bang210 <95253075+Bang210@users.noreply.github.com>

--- src/main/java/com/ll/demo/article/controller/ArticleController.java
+++ src/main/java/com/ll/demo/article/controller/ArticleController.java
@@ -1,26 +1,24 @@
 package com.ll.demo.article.controller;
 
 import com.ll.demo.article.dto.ArticleDetailResponseDto;
+import com.ll.demo.article.dto.ArticleListResponseDto;
 import com.ll.demo.article.dto.ArticleRequestDto;
 import com.ll.demo.article.entity.Article;
-import com.ll.demo.article.entity.Image;
-import com.ll.demo.article.entity.Tag;
 import com.ll.demo.article.service.ArticleService;
 import com.ll.demo.article.service.ImageService;
+import com.ll.demo.article.service.TagService;
 import com.ll.demo.global.response.GlobalResponse;
 import com.ll.demo.member.entity.Member;
 import com.ll.demo.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,29 +27,43 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final MemberService memberService;
+    private final TagService tagService;
 
     private final ImageService imageService;
 
     //전체 글 조회
     @GetMapping("")
-    public GlobalResponse findAll(@RequestParam(defaultValue = "0") int page) {
+    public GlobalResponse findAllArticles() {
 
-        Page<Article> articles = articleService.findAll(page);
-        return GlobalResponse.of("200", "paging success", articles);
+        Set<ArticleListResponseDto> articleListResponseDtoSet = articleService.findAll()
+                .stream()
+                .map(article -> new ArticleListResponseDto(article))
+                .collect(Collectors.toSet());
+        return GlobalResponse.of("200", "success", articleListResponseDtoSet);
     }
 
     //단일 글 조회
-    @GetMapping("/{id}")
-    public GlobalResponse show(@PathVariable("id") Long id) {
+    @GetMapping("/detail/{id}")
+    public GlobalResponse showArticle(@PathVariable("id") Long id) {
 
         Article article = articleService.getArticleById(id);
         return GlobalResponse.of("200", "success", new ArticleDetailResponseDto(article));
     }
 
+    //tag값으로 글 검색
+    @GetMapping("/{tagName}")
+    public GlobalResponse searchArticlesByTag(@PathVariable("tagName") String tagName) {
+        Set<ArticleListResponseDto> articleListResponseDtoSet = tagService.getArticlesByTagName(tagName)
+                .stream()
+                .map(article -> new ArticleListResponseDto(article))
+                .collect(Collectors.toSet());
+        return GlobalResponse.of("200", "success", articleListResponseDtoSet);
+    }
+
     // 글 생성
     @PreAuthorize("isAuthenticated()")
     @PostMapping("")
-    public GlobalResponse create(
+    public GlobalResponse createArticle(
             @Valid ArticleRequestDto articleRequestDto,
             Principal principal) throws IOException {
         // 사용자 인증 정보 가져오기
@@ -70,7 +82,7 @@ public class ArticleController {
     //글 수정
     @PreAuthorize("isAuthenticated()")
     @PutMapping("/{id}")
-    public GlobalResponse update(
+    public GlobalResponse updateArticle(
             @PathVariable("id") Long id,
             Principal principal,
             @Valid ArticleRequestDto articleRequestDto
@@ -100,7 +112,7 @@ public class ArticleController {
     //글 삭제
     @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{id}")
-    public GlobalResponse delete(@PathVariable("id") Long id, Principal principal) throws IOException {
+    public GlobalResponse deleteArticle(@PathVariable("id") Long id, Principal principal) throws IOException {
 
         Article article = articleService.getArticleById(id);
         Member member = memberService.findByUsername(principal.getName());
