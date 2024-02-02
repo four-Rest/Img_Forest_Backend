75306aee77974e7591e983f4872b3b838f012835
tallguy188 <cksghd4220@naver.com>
2024-02-02 14:28:28 +0900

    setting

--- frontapp/src/lib/login_stores.js
+++
@@ -1,5 +0,0 @@
-// 전역상태관리에 사용되는 스벨트의 store
-
-import {writable} from "svelte/store";
-
-export let isLogin= writable(false);  // writable-> 값이 변경 가능한 store 생성
--- frontapp/src/routes/article/+page.svelte
+++
@@ -1,5 +0,0 @@
-<section>
-
-     <div> 게시물페이지입니다. </div>
-
-</section>
