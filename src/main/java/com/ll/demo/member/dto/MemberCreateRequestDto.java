c53f8dc4fcdcb7842927f1e1616f5dd488f34292
OhJeMIN <jasonoh22@naver.com>
2024-02-05 21:46:22 +0900

    feat: 카카오 소셜 로그인 구현
    카카오 소셜 로그인 구현 및 소셜 로그인을 위한 DTo 생성, 소셜 로그인을
    위한 SecurityUser 생성

---
+++ src/main/java/com/ll/demo/member/dto/MemberCreateRequestDto.java
@@ -0,0 +1,29 @@
+package com.ll.demo.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberCreateRequestDto {
+    @Size(min = 3, max = 25)
+    @NotEmpty(message = "사용자ID는 필수항목입니다.")
+    private String username;
+
+    // 기본 패스워드
+    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    private String password1;
+
+    // 확인 패스워드
+    @NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+    private String password2;
+
+    @NotEmpty(message = "이메일은 필수항목입니다.")
+    @Email
+    private String email;
+
+    private String nickname;
+}
