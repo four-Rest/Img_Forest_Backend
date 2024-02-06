c6fcb01617bdf24595cccefe3edfdfa566d38035
OhJeMIN <jasonoh22@naver.com>
2024-02-06 18:20:01 +0900

    docs: daisyUI 를 위한 css 및 tailwind 수정
    daisyUI를 위한 css 및 tailwind 수정

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -2,16 +2,16 @@
 import './App.css'; // 스타일 시트
 import React from 'react';
 import Router from "./components/layouts/Router";
-
-
-
+import { AuthProvider } from './api/AuthContext';
 
 function App() {
 
   return (
+    <AuthProvider>
       <Router>
     
       </Router>
+    </AuthProvider>
   );
 }
 
