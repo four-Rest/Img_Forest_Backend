0872f762e7e6f5b9c7a12829397d76d85434a737
Jeong jinyoung <jjyoung2419@gmail.com>
2024-02-06 18:08:25 +0900

    feat: Article.js

--- react_front/src/components/layouts/Router.js
+++ react_front/src/components/layouts/Router.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Article from "./Article";
 
 function Router() {
     return (
       <BrowserRouter>
           <Routes>
             <Route path="/home" element={<Home />} />
+            <Route path="/article" element={<Article />} />
+
           </Routes>
       </BrowserRouter>
     );
