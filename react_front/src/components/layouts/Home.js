b2f9100074f7bfacd3fc9a3f25f71251e39e2a94
tallguy188 <cksghd4220@naver.com>
2024-02-06 21:22:26 +0900

    feat: 무한스크롤 적용
    무한스크롤 적용

--- react_front/src/components/layouts/Home.js
+++ react_front/src/components/layouts/Home.js
@@ -1,26 +1,62 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import './styles.css';
 
 
 function Home() {
   const [articleData, setArticleData] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  const [page,setPage] = useState(1);
   const apiBaseUrl = process.env.REACT_APP_CORE_API_BASE_URL;
 
+  const target = useRef(null);
+
   useEffect(() => {
-    const fetchArticleData = async () => {
+    const fetchData = async () => {
+      setLoading(true);
       try {
-        const res = await fetch(`/api/article`);
+        const res = await fetch(`/api/article?page=${page}`);
         const data = await res.json();
-        setArticleData(data.data);
-      } catch (error) {
-        console.error('Error fetching article data:', error);
+        setArticleData(prevData => [...prevData, ...data.data]);
+        setPage(prevPage => prevPage +1);
+      }catch(error) {
+        console.error('Error fetching article data', error);
       }
+
+      setLoading(false);
     };
 
-    fetchArticleData();
+    const observer = new IntersectionObserver((entries) => {
+
+      if(entries[0].isIntersecting) {
+        fetchData();
+      }
+    }, {threshold :1 });
+
+    if(target.current) {
+      observer.observe(target.current);
+    }
+    return () => {
+      if(target.current) {
+        observer.unobserve(target.current);
+      }
+    };
+  }, [apiBaseUrl,page]);
+  // useEffect(() => {
+  //   const fetchArticleData = async () => {
+  //     try {
+  //       const res = await fetch(`/api/article`);
+  //       const data = await res.json();
+  //       setArticleData(data.data);
+  //     } catch (error) {
+  //       console.error('Error fetching article data:', error);
+  //     }
+  //   };
+
+  //   fetchArticleData();
 
     
-  }, [apiBaseUrl]);
+  // }, [apiBaseUrl]);
 
   return (
       <div className="container">
@@ -29,6 +65,8 @@ function Home() {
                 <img src={`imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
               </div>
           ))}
+          {loading && <div>Loading...</div>}
+          <div ref = {target}></div>
       </div>
   );
 };
