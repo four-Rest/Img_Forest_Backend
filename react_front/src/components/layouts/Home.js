ae3e0a5d383925186ca7811eb33d6205641e32b5
tallguy188 <cksghd4220@naver.com>
2024-02-07 16:25:39 +0900

    refactor: 이미지 렌더링 로직 변경 이미지 렌더링 로직 변경

--- react_front/src/components/layouts/Home.js
+++ react_front/src/components/layouts/Home.js
@@ -4,34 +4,36 @@ import './styles.css';
 function Home() {
   const [articleData, setArticleData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(10);
   const apiBaseUrl = process.env.REACT_APP_CORE_API_BASE_URL;
 
   const target = useRef(null);
-
-
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await fetch(`/api/article?page=${page}`);
+        const res = await fetch(`/api/article`);
         const data = await res.json();
-        setArticleData(prevData => shuffleArray([...prevData, ...data.data]));
-        setPage(prevPage => prevPage + 1);
+        setArticleData(data.data);
       } catch (error) {
         console.error('Error fetching article data:', error);
       }
       setLoading(false);
     };
+    fetchData();
+  }, [apiBaseUrl]);
 
+  useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        fetchData();
+      if (entries[0].isIntersecting && !loading) {
+        setLoading(true);
+        setEndIndex(prevEndIndex => prevEndIndex + 10); // 스크롤이 끝에 도달하면 endIndex를 증가시켜 새로운 데이터를 불러옴
       }
     }, { threshold: 1 });
 
-    if (target.current) {
+    if (!loading && target.current) {
       observer.observe(target.current);
     }
 
@@ -40,26 +42,15 @@ function Home() {
         observer.unobserve(target.current);
       }
     };
-  }, [apiBaseUrl, page]);
-
-   // 배열을 섞어주는 함수
-   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
+  }, [loading]); // loading이 변경될 때만 useEffect 실행
 
   return (
     <div className="container">
-      {articleData.map((article, index) => (
-        <div className="box">
+      {articleData.slice(startIndex, endIndex).map((article, index) => (
+        <div key={index} className="box">
           <img src={`imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
         </div>
       ))}
-      {loading && <div>Loading...</div>}
       <div ref={target}></div>
     </div>
   );
