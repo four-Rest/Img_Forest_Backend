f3a66db7ecc433dcb7a008cb7636e86a1654de59
Bang210 <bangvelope@gmail.com>
2024-02-09 17:40:48 +0900

    update

--- react_front/src/components/layouts/Home.js
+++ react_front/src/components/layouts/Home.js
@@ -55,11 +55,9 @@ function Home() {
   return (
     <div className="container">
       {articleData.slice(startIndex, endIndex).map((article) => (
-          <li key={article.id} >
-            <div className="box">
-              <img src={`imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
-            </div>
-          </li>
+        <div key={article.id} className="box">
+          <img src={`imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
+        </div>
       ))}
       <div ref={target}></div> 
     </div>
