d282821935757cf5d791192131a458e328078c35
Bang210 <bangvelope@gmail.com>
2024-02-12 18:27:45 +0900

    feat: webconfig

--- src/main/java/com/ll/demo/global/config/WebConfig.java
+++ src/main/java/com/ll/demo/global/config/WebConfig.java
@@ -4,6 +4,7 @@ package com.ll.demo.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders(HttpHeaders.LOCATION) // 리다이렉트에 사용하는 LOCATION 헤더 활성화
                 .allowCredentials(true)
                 .maxAge(10800);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/imgFiles/**")
+                .addResourceLocations("file:/app/imgFiles/");
     }
 }
