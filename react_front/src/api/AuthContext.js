b9d5468fe8131ffbfdf3c7e07e148afabf2a1d76
OhJeMIN <jasonoh22@naver.com>
2024-02-11 17:47:12 -1000

    fix: 로그아웃 수정

--- react_front/src/api/AuthContext.js
+++ react_front/src/api/AuthContext.js
@@ -6,7 +6,7 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
-
+  const apiUrl = process.env.REACT_APP_CORE_API_BASE_URL;
   const login = () => setIsLogin(true);
 
   function deleteCookie(name) {
@@ -16,15 +16,29 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("username");
     localStorage.removeItem("nickname");
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    deleteCookie("accessToken");
-    deleteCookie("refreshToken");
-    deleteCookie("JSESSIONID"); 
+    localStorage.removeItem("isLogin");
+    fetch(`${apiUrl}/api/member/logout`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  });
     setIsLogin(false)
     window.location.href = `/`;
     ;
   };
+
+  function getTokenExpiry(token) {
+    const payload = token.split('.')[1]; // 토큰의 두 번째 부분(payload)를 추출
+    const decodedPayload = atob(payload); // Base64Url 디코드
+    const payloadObj = JSON.parse(decodedPayload); // JSON 문자열을 객체로 변환
+    const expiry = payloadObj.exp; // exp 필드 값 추출
+  
+    return new Date(expiry * 1000); // JavaScript Date 객체로 변환 (밀리초 단위로 변환 필요)
+  }
+
+  
   return (
     <AuthContext.Provider value={{ isLogin, login, logout }}>
       {children}
