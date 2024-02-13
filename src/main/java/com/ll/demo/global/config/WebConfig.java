ab91b35e6973a9cd0577b47e3736a9707ac44b84
Bang210 <bangvelope@gmail.com>
2024-02-13 20:48:24 +0900

    test

--- src/main/java/com/ll/demo/global/config/WebConfig.java
+++ src/main/java/com/ll/demo/global/config/WebConfig.java
@@ -10,20 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://*.for-rest.site")
+                .allowedOrigins("https://img.for-rest.site")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.LOCATION) // 리다이렉트에 사용하는 LOCATION 헤더 활성화
                 .allowCredentials(true)
                 .maxAge(10800);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imgFiles/**")
-                .addResourceLocations("file:/app/react_fornt/public/imgFiles/**");
     }
 }
