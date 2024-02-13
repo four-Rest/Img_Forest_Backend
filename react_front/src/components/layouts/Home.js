e2161a8f522230a102ed669bb552c1fe11775d8c
Bang210 <bangvelope@gmail.com>
2024-02-13 11:49:52 +0900

    test

--- react_front/src/components/layouts/Home.js
+++ react_front/src/components/layouts/Home.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './styles.css'; 
+import './styles.css';
 
-function Home() { 
-  
+function Home() {
+
   const [articleData, setArticleData] = useState([]); // 이미지 데이터 배열
   const [loading, setLoading] = useState(false); // 로딩 상태
   const [startIndex, setStartIndex] = useState(0); // 표시할 이미지의 시작 인덱스
@@ -26,19 +26,20 @@ function Home() {
       }
       setLoading(false); // 로딩 상태를 false로 설정
     };
+    fetchData(); // fetchData 함수 호출
   }, [apiBaseUrl]); // apiBaseUrl이 변경될 때만 useEffect 실행
 
   useEffect(() => {
     // useEffect를 사용하여 target 요소의 가시성을 감시하고 스크롤 시 추가 데이터 로드
     const observer = new IntersectionObserver(
-      async(entries) => {
-        if (entries[0].isIntersecting && !loading) { // target 요소가 화면에 나타났고 로딩 중이 아닌 경우
-          setLoading(true); // 로딩 상태를 true로 설정
-          // setEndIndex(prevEndIndex => Math.max(prevEndIndex + 10, Math.min(prevEndIndex + 10, articleData.length))); // endIndex를 업데이트하여 추가 이미지를 표시
-          setEndIndex(prevEndIndex => Math.min(prevEndIndex + 10, articleData.length));
-          // 현재 ArticleData.length=45, 어떨때는 40개 렌더링 어떨때는 50개 렌더링
-        }
-      }, { threshold: 0 }); // IntersectionObserver 옵션 설정
+        async(entries) => {
+          if (entries[0].isIntersecting && !loading) { // target 요소가 화면에 나타났고 로딩 중이 아닌 경우
+            setLoading(true); // 로딩 상태를 true로 설정
+            setEndIndex(prevEndIndex => Math.max(prevEndIndex + 10, Math.min(prevEndIndex + 10, articleData.length))); // endIndex를 업데이트하여 추가 이미지를 표시
+            //setEndIndex(prevEndIndex => Math.min(prevEndIndex + 10, articleData.length));
+            // 현재 ArticleData.length=45, 어떨때는 40개 렌더링 어떨때는 50개 렌더링
+          }
+        }, { threshold: 0 }); // IntersectionObserver 옵션 설정
 
     if (!loading && target.current) { // 로딩 중이 아니고 target이 현재 존재하는 경우
       observer.observe(target.current); // IntersectionObserver를 target에 등록하여 가시성 감시
@@ -52,18 +53,15 @@ function Home() {
   }, [loading, articleData]); // loading 또는 articleData가 변경될 때만 useEffect 실행
 
   return (
-    <div className="container">
-      {(
-        articleData.slice(startIndex, endIndex).map((article) => (
-          <div key={article.id} className="box">
-            <img src={`https://api.img.for-rest.site/api/article/image/${article.imgFileName}`} alt="" />
-          </div>
-        ))
-      )}
-      <div ref={target}></div>
-    </div>
+      <div className="container">
+        {articleData.slice(startIndex, endIndex).map((article) => (
+            <div key={article.id} className="box">
+              <img src={`api/article/image/${article.imgFilePath}/${article.imgFileName}`} alt="" />
+            </div>
+        ))}
+        <div ref={target}></div>
+      </div>
   );
 }
-
 
 export default Home; // Home 컴포넌트를 내보냄
