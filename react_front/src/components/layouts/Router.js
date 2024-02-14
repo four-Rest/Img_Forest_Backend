27265c00a57ca71514bb6f2478e7cba0f4a4b867
tallguy188 <cksghd4220@naver.com>
2024-02-14 21:48:06 +0900

    refactor: SearchTagProvider, IdDetailProvider 이동
    Router.js -> App.js

--- react_front/src/components/layouts/Router.js
+++ react_front/src/components/layouts/Router.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
 import Article from "./Article";
@@ -8,21 +7,20 @@ import CheckSocialLogin from "../../api/CheckSocialLogin";
 import HomePaging from "./HomePaging";
 import Detail from "./Detail";
 import {SearchTagProvider} from '../../api/SearchTagContext';
+import { IdDetailProvider } from '../../api/IdDetailContext';
 
 function Router() {
   return (
     <BrowserRouter>
-      <SearchTagProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePaging />} />
-          <Route path="/article" element={<Article />} />
-          <Route path="/article/:tagString" element={<ArticleTag />} />
-          <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/check-social-login" element={<CheckSocialLogin />} />
-        </Routes>
-        <Footer />
-      </SearchTagProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePaging />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/article/:tagString" element={<ArticleTag />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/check-social-login" element={<CheckSocialLogin />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
