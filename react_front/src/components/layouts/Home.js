17631befc4c89a789b2af8f6e539ea35abfdeb32
Bang210 <bangvelope@gmail.com>
2024-02-14 13:51:56 +0900

    test

--- react_front/src/components/layouts/Home.js
+++ react_front/src/components/layouts/Home.js
@@ -56,7 +56,7 @@ function Home() {
       <div className="container">
         {articleData.slice(startIndex, endIndex).map((article) => (
             <div key={article.id} className="box">
-              <img src={`/imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
+              <img src={`imgFiles/${article.imgFilePath}/${article.imgFileName}`} alt="" />
             </div>
         ))}
         <div ref={target}></div>
