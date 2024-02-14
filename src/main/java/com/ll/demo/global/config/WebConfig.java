3291a9f85d86a6d1b2fd8b81027328b8fc6de743
Bang210 <bangvelope@gmail.com>
2024-02-14 14:45:47 +0900

    gen

--- src/main/java/com/ll/demo/global/config/WebConfig.java
+++ src/main/java/com/ll/demo/global/config/WebConfig.java
@@ -6,19 +6,24 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.ll.demo.global.app.AppConfig;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://img.for-rest.site")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
-                .exposedHeaders(HttpHeaders.LOCATION, HttpHeaders.CONTENT_TYPE) // 리다이렉트에 사용하는 LOCATION 헤더 활성화
+                .exposedHeaders(HttpHeaders.LOCATION) // 리다이렉트에 사용하는 LOCATION 헤더 활성화
                 .allowCredentials(true)
                 .maxAge(10800);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/gen/**")
+                .addResourceLocations("file:///" + AppConfig.getGenFileDirPath() + "/");
     }
 }
