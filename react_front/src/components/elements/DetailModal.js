155efcdd1cb3273aa3f9b97451fdd4659b9f21b1
OhJeMIN <jasonoh22@naver.com>
2024-02-15 21:04:47 +0900

    feat: 댓글 작성 기능 추가

--- react_front/src/components/elements/DetailModal.js
+++ react_front/src/components/elements/DetailModal.js
@@ -7,12 +7,14 @@ import {
   faHeartCrack,
   faComment,
   faTag,
+  faPaperPlane,
   comments,
 } from "@fortawesome/free-solid-svg-icons";
 
 function DetailModal({ showModal, setShowModal, articleId }) {
   const [detail, setDetail] = useState(null);
   const [comment, setComment] = useState("");
+  const [currentComments, setCurrentComments] = useState([]);
   const apiUrl = process.env.REACT_APP_CORE_API_BASE_URL;
 
   useEffect(() => {
@@ -35,6 +37,7 @@ function DetailModal({ showModal, setShowModal, articleId }) {
         }
         const res = await response.json();
         setDetail(res.data);
+        console.log(res.data);
       } catch (error) {
         console.error("Error fetching article detail:", error);
       }
@@ -93,10 +96,45 @@ function DetailModal({ showModal, setShowModal, articleId }) {
 
   //댓글 작성
   const handleCommentSubmit = async () => {
-    // 서버로 댓글 전송 로직 구현
-    console.log(comment); // 입력된 댓글 확인
-    // 댓글 전송 후 입력 필드 초기화
-    setComment("");
+    try {
+      const name = localStorage.getItem("username");
+      const response = await fetch(`${apiUrl}/api/comment/`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          articleId: articleId,
+          username: name,
+          content: comment,
+        }),
+      });
+
+      if (response.ok) {
+        const newCommentData = await response.json(); // 새 댓글 데이터
+        const newComment = {
+          ...newCommentData.data,
+          username: name, // 새 댓글 객체에 username 추가
+        };
+
+        // 새 댓글을 목록에 추가
+        setDetail((prevDetail) => ({
+          ...prevDetail,
+          listCommentResponses: [
+            ...prevDetail.listCommentResponses,
+            newComment,
+          ],
+        }));
+        setComment(""); // 입력 필드 초기화
+      } else {
+        console.error("댓글 생성 실패");
+        // 실패 시 사용자에게 알림
+      }
+    } catch (error) {
+      console.error("에러 발생:", error);
+      // 네트워크 오류 처리
+    }
   };
 
   if (!showModal) return null;
@@ -131,6 +169,27 @@ function DetailModal({ showModal, setShowModal, articleId }) {
   const handleDownload = () => {
     const imagePath = `/imgFiles/${imgFilePath}/${imgFileName}`;
     downloadImage(imagePath, imgFileName);
+  };
+
+  const calculateTimeAgo = (createdDate) => {
+    const date1 = new Date(createdDate);
+    const date2 = new Date();
+    const difference = date2 - date1;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(difference / (1000 * 60 * 60));
+    const minutes = Math.floor(difference / (1000 * 60));
+    const seconds = Math.floor(difference / 1000);
+
+    if (days >= 1) {
+      return `${days}일`;
+    } else if (hours >= 1) {
+      return `${hours}시간`;
+    } else if (minutes >= 1) {
+      return `${minutes}분`;
+    } else {
+      return `${seconds}초`;
+    }
   };
 
   return (
@@ -203,6 +262,19 @@ function DetailModal({ showModal, setShowModal, articleId }) {
             <FontAwesomeIcon icon={faComment} />
             댓글 {listCommentResponses.length}개
           </div>
+          <div>
+            <ul>
+              {listCommentResponses.map((comment, index) => (
+                <li key={index}>
+                  <strong>{comment.username}</strong>: {comment.content}
+                  <br />
+                  <span className="commentTime">
+                    {calculateTimeAgo(comment.createdDate)}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div
             className="comment-input-container"
             style={{
@@ -210,6 +282,8 @@ function DetailModal({ showModal, setShowModal, articleId }) {
               background: "#f0f0f0",
               borderRadius: "5px",
               padding: "10px",
+              display: "flex",
+              alignItems: "center",
             }}
           >
             <input
@@ -218,11 +292,12 @@ function DetailModal({ showModal, setShowModal, articleId }) {
               onChange={(e) => setComment(e.target.value)}
               placeholder="댓글을 입력하세요..."
               style={{
-                width: "100%",
+                flex: 1,
                 padding: "10px",
                 border: "none",
                 borderRadius: "5px",
                 background: "#e9ecef",
+                marginRight: "10px", // 버튼과의 간격 조정
               }}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
@@ -231,6 +306,24 @@ function DetailModal({ showModal, setShowModal, articleId }) {
                 }
               }}
             />
+            {comment && (
+              <button
+                onClick={handleCommentSubmit}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "10px",
+                  border: "none",
+                  borderRadius: "5px",
+                  background: "#007bff",
+                  color: "white",
+                  cursor: "pointer",
+                }}
+              >
+                <FontAwesomeIcon icon={faPaperPlane} />
+              </button>
+            )}
           </div>
         </div>
       </div>
