6853172fa2dff65894dd07e3cb8d639df13ecf23
Bang210 <bangvelope@gmail.com>
2024-02-16 10:11:36 +0900

    q

--- react_front/src/components/layouts/HomePaging.js
+++ react_front/src/components/layouts/HomePaging.js
@@ -29,36 +29,36 @@ function HomePaging() {
     console.log(id);
   };
 
-  useEffect(() => {
-    // 서버에 accessToken 검증 요청
-    fetch(`${apiBaseUrl}/api/member/checkAccessToken`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-        .then((response) => response.json())
-        .then((data) => {
-          // 서버 응답에서 resultCode와 LoginResponseDto 정보를 확인
-          if (data.resultCode === "200") {
-            // 로그인 성공 처리
-            login();
-            console.log("로그인 성공");
-            localStorage.setItem("username", data.data.username); // 응답에서 username 추출
-            localStorage.setItem("isLogin", "true");
-            navigate("/", { replace: true }); // 홈으로 리디렉션
-          } else {
-            // 로그인 실패 처리 (유효하지 않은 토큰, 토큰 만료 등)
-            console.log(data.message); // 서버에서 보낸 오류 메시지 출력
-            logout(); // 로그아웃 처리
-          }
-        })
-        .catch((error) => {
-          console.error("에러 발생:", error);
-          logout(); // 네트워크 오류 등의 이유로 로그아웃 처리
-        });
-  }, []);
+  // useEffect(() => {
+  //   // 서버에 accessToken 검증 요청
+  //   fetch(`${apiBaseUrl}/api/member/checkAccessToken`, {
+  //     method: "POST",
+  //     credentials: "include",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         // 서버 응답에서 resultCode와 LoginResponseDto 정보를 확인
+  //         if (data.resultCode === "200") {
+  //           // 로그인 성공 처리
+  //           login();
+  //           console.log("로그인 성공");
+  //           localStorage.setItem("username", data.data.username); // 응답에서 username 추출
+  //           localStorage.setItem("isLogin", "true");
+  //           navigate("/", { replace: true }); // 홈으로 리디렉션
+  //         } else {
+  //           // 로그인 실패 처리 (유효하지 않은 토큰, 토큰 만료 등)
+  //           console.log(data.message); // 서버에서 보낸 오류 메시지 출력
+  //           logout(); // 로그아웃 처리
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error("에러 발생:", error);
+  //         logout(); // 네트워크 오류 등의 이유로 로그아웃 처리
+  //       });
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
