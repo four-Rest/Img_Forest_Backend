6e7206f986fb91a07c8a8a30852a7ce08d3d764c
Bang210 <bangvelope@gmail.com>
2024-02-16 14:31:03 +0900

    s

--- react_front/src/components/layouts/HomePaging.js
+++ react_front/src/components/layouts/HomePaging.js
@@ -22,7 +22,6 @@ function HomePaging() {
   const navigate = useNavigate();
   const apiUrl = process.env.REACT_APP_CORE_API_BASE_URL;
   const { login, logout } = useAuth();
-  const [isLoginChecked, setIsLoginChecked] = useState(false);
 
   const handleImageClick = (id) => {
     setIdDetail(id);
@@ -32,8 +31,8 @@ function HomePaging() {
   };
 
   useEffect(() => {
-    // const searchParams = new URLSearchParams(location.search);
-    if (!isLoginChecked) {
+    const searchParams = new URLSearchParams(location.search);
+    if (searchParams.has("check-social-login")) {
       // 서버에 accessToken 검증 요청
       fetch(`${apiUrl}/api/member/checkAccessToken`, {
         method: "POST",
@@ -42,29 +41,26 @@ function HomePaging() {
           "Content-Type": "application/json",
         },
       })
-          .then((response) => response.json())
-          .then((data) => {
-            // 서버 응답에서 resultCode와 LoginResponseDto 정보를 확인
-            if (data.resultCode === "200") {
-              // 로그인 성공 처리
-              login();
-              setIsLoginChecked(true);
-              console.log("로그인 성공");
-              localStorage.setItem("username", data.data.username); // 응답에서 username 추출
-              localStorage.setItem("isLogin", "true");
-              navigate("/", { replace: true }); // 홈으로 리디렉션
-            } else {
-              // 로그인 실패 처리 (유효하지 않은 토큰, 토큰 만료 등)
-              console.log(data.message); // 서버에서 보낸 오류 메시지 출력
-              logout(); // 로그아웃 처리
-              setIsLoginChecked(true);
-            }
-          })
-          .catch((error) => {
-            console.error("에러 발생:", error);
-            logout(); // 네트워크 오류 등의 이유로 로그아웃 처리
-            setIsLoginChecked(true);
-          });
+        .then((response) => response.json())
+        .then((data) => {
+          // 서버 응답에서 resultCode와 LoginResponseDto 정보를 확인
+          if (data.resultCode === "200") {
+            // 로그인 성공 처리
+            login();
+            console.log("로그인 성공");
+            localStorage.setItem("username", data.data.username); // 응답에서 username 추출
+            localStorage.setItem("isLogin", "true");
+            navigate("/", { replace: true }); // 홈으로 리디렉션
+          } else {
+            // 로그인 실패 처리 (유효하지 않은 토큰, 토큰 만료 등)
+            console.log(data.message); // 서버에서 보낸 오류 메시지 출력
+            logout(); // 로그아웃 처리
+          }
+        })
+        .catch((error) => {
+          console.error("에러 발생:", error);
+          logout(); // 네트워크 오류 등의 이유로 로그아웃 처리
+        });
     }
   }, [location.search]);
 
