0424542f4a3d2ac822ed5fb1918c3acb2a231b3d
OhJeMIN <jasonoh22@naver.com>
2024-02-23 15:36:06 +0900

    fix: 카카오 로그인 수정
    카카오 로그인 시 리다이렉션 수정 url에 있는 check-social-login 삭제로

--- react_front/src/components/layouts/HomePaging.js
+++ react_front/src/components/layouts/HomePaging.js
@@ -50,7 +50,10 @@ function HomePaging() {
             console.log("로그인 성공");
             localStorage.setItem("username", data.data.username); // 응답에서 username 추출
             localStorage.setItem("isLogin", "true");
-            navigate("/", { replace: true }); // 홈으로 리디렉션
+
+            const url = new URL(window.location);
+            url.searchParams.delete("check-social-login");
+            window.history.replaceState({}, "", url);
           } else {
             // 로그인 실패 처리 (유효하지 않은 토큰, 토큰 만료 등)
             console.log(data.message); // 서버에서 보낸 오류 메시지 출력
@@ -69,21 +72,22 @@ function HomePaging() {
       setLoading(true);
       try {
         const res = await fetch(
-            `${apiBaseUrl}/api/article?pageNo=${pageNo}&pageSize=${pageSize}`, {
-          method: "GET",
-              credentials: "include",
-              headers: {
-            "Content-Type": "application/json",
-          },
-        }
+          `${apiBaseUrl}/api/article?pageNo=${pageNo}&pageSize=${pageSize}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
         );
         const data = await res.json();
         console.log(data.data);
         // setArticleData(prevData => prevData.concat(data.data));
         setArticleData((prevData) => {
           const newData = data.data.filter(
-              (newArticle) =>
-                  !prevData.some((prevArticle) => prevArticle.id === newArticle.id)
+            (newArticle) =>
+              !prevData.some((prevArticle) => prevArticle.id === newArticle.id)
           );
           return [...prevData, ...newData];
         });
@@ -99,13 +103,13 @@ function HomePaging() {
   useEffect(() => {
     // useEffect를 사용하여 target 요소의 가시성을 감시하고 스크롤 시 추가 데이터 로드
     const observer = new IntersectionObserver(
-        async (entries) => {
-          if (entries[0].isIntersecting && !loading && pageNo < totalPages - 1) {
-            // target 요소가 화면에 나타났고 로딩 중이 아닌 경우
-            setPageNo((prevPageNo) => prevPageNo + 1);
-          }
-        },
-        { threshold: 0 }
+      async (entries) => {
+        if (entries[0].isIntersecting && !loading && pageNo < totalPages - 1) {
+          // target 요소가 화면에 나타났고 로딩 중이 아닌 경우
+          setPageNo((prevPageNo) => prevPageNo + 1);
+        }
+      },
+      { threshold: 0 }
     ); // IntersectionObserver 옵션 설정
 
     if (!loading && target.current) {
@@ -122,25 +126,25 @@ function HomePaging() {
 
   // 클릭시 modal로 redirection
   return articleData.length !== 0 ? (
-      <div className="container pt-24">
-        {articleData.map((article) => (
-            <div key={article.id} className="box">
-              <img
-                  src={`${apiBaseUrl}/gen/${article.imgFilePath}/${article.imgFileName}`}
-                  alt="a"
-                  onClick={() => handleImageClick(article.id)}
-              />
-            </div>
-        ))}
-        <div ref={target}></div>
-        <DetailModal
-            showModal={showDetailModal}
-            setShowModal={setShowDetailModal}
-            articleId={idDetail}
-        />
-      </div>
+    <div className="container pt-24">
+      {articleData.map((article) => (
+        <div key={article.id} className="box">
+          <img
+            src={`${apiBaseUrl}/gen/${article.imgFilePath}/${article.imgFileName}`}
+            alt="a"
+            onClick={() => handleImageClick(article.id)}
+          />
+        </div>
+      ))}
+      <div ref={target}></div>
+      <DetailModal
+        showModal={showDetailModal}
+        setShowModal={setShowDetailModal}
+        articleId={idDetail}
+      />
+    </div>
   ) : (
-      <></>
+    <></>
   );
 }
 
--- react_front/src/components/layouts/Router.js
+++ react_front/src/components/layouts/Router.js
@@ -6,26 +6,26 @@ import ArticleTag from "./ArticleTag";
 import CheckSocialLogin from "../../api/CheckSocialLogin";
 import HomePaging from "./HomePaging";
 import Detail from "./Detail";
-import {SearchTagProvider} from '../../api/SearchTagContext';
-import { IdDetailProvider } from '../../api/IdDetailContext';
+import { SearchTagProvider } from "../../api/SearchTagContext";
+import { IdDetailProvider } from "../../api/IdDetailContext";
 import MyArticle from "./MyArticle";
 import Modify from "./Modify";
 function Router() {
   return (
     <div className="flex flex-col min-h-screen">
-    <BrowserRouter>
-      <Header />
-      <Routes className="flex-grow">
-        <Route path="/" element={<HomePaging />} />
-        <Route path="/article" element={<Article />} />
-        <Route path="/modify/:id" element={<Modify />} />
-        <Route path="/article/:tagString" element={<ArticleTag />} />
-        <Route path="/myarticle/:userNick" element={<MyArticle />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/check-social-login" element={<CheckSocialLogin />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes className="flex-grow">
+          <Route path="/" element={<HomePaging />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="/modify/:id" element={<Modify />} />
+          <Route path="/article/:tagString" element={<ArticleTag />} />
+          <Route path="/myarticle/:userNick" element={<MyArticle />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          {/* <Route path="/check-social-login" element={<CheckSocialLogin />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
