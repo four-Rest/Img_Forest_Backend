51a69eb32d6897dabba01da3ef8ca98599698b9e
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2024-02-23 17:11:39 +0900

    Revert "client -> main (#13)"
    This reverts commit c6dab0c68a0929654ecd51b0224ef1d8567979a2.

--- src/main/java/com/ll/demo/DemoApplication.java
+++ src/main/java/com/ll/demo/DemoApplication.java
@@ -2,17 +2,12 @@ package com.ll.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
-@EnableJpaAuditing
-public class DemoApplication
-{
+public class DemoApplication {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        }
+    }
 
-        }
+}
--- src/main/java/com/ll/demo/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.demo.comment.repository;
-
-import com.ll.demo.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
