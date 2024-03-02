7f141b55bd61360afb218807d7606895a214cb3a
tallguy188 <cksghd4220@naver.com>
2024-03-02 09:50:33 +0900

    fix: JPA 설정 수정 설정 수정

--- src/main/java/com/ll/demo/order/repository/OrderRepository.java
+++ src/main/java/com/ll/demo/order/repository/OrderRepository.java
@@ -3,6 +3,6 @@ package com.ll.demo.order.repository;
 import com.ll.demo.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Long, Order> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
