d96aca8d1943c948698e2c14e31c38a7b22ee8d6
tallguy188 <cksghd4220@naver.com>
2024-03-02 09:50:48 +0900

    feat: saveOrder 추가 add saveOrder

--- src/main/java/com/ll/demo/payment/service/PaymentService.java
+++ src/main/java/com/ll/demo/payment/service/PaymentService.java
@@ -1,14 +1,41 @@
 package com.ll.demo.payment.service;
 
 
+import com.ll.demo.article.entity.Article;
+import com.ll.demo.article.repository.ArticleRepository;
+import com.ll.demo.member.entity.Member;
+import com.ll.demo.member.repository.MemberRepository;
+import com.ll.demo.member.service.MemberService;
 import com.ll.demo.order.dto.OrderRequestDto;
+import com.ll.demo.order.entity.Order;
+import com.ll.demo.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PaymentService {
+    private final OrderRepository orderRepository;
+    private final ArticleRepository articleRepository;
 
-    public void saveOrder(Long userId, OrderRequestDto orderRequestDto) {
+    public void saveOrder(Member member, OrderRequestDto orderRequestDto) {
+        Article article = articleRepository.findArticleById(orderRequestDto.getArticleId())
+                .orElseThrow(() -> new RuntimeException("게시물이 존재하지 않습니다."));
+        Order order = Order.builder()
+                .member(member)
+                .article(article)
+                .orderNumber(orderRequestDto.getOrderNumber())
+                .orderDate(new Timestamp(System.currentTimeMillis()))
+                .paymentMethod(orderRequestDto.getPaymentMethod()).build();
+
+        orderRepository.save(order);
+
     }
 }
