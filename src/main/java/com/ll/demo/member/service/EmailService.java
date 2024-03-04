dd38a0ffcaaf76dbf99fedb8a7bdab9a02b5aca4
OhJeMIN <jasonoh22@naver.com>
2024-03-04 18:10:22 +0900

    feat: 인증코드 이메일로 보내기 구현
    6자리 영문자와 숫자로 이루어진 랜덤 인증 코드를 전송
    전송 후 db에 저장

--- src/main/java/com/ll/demo/member/service/EmailService.java
+++ src/main/java/com/ll/demo/member/service/EmailService.java
@@ -3,37 +3,42 @@ package com.ll.demo.member.service;
 import com.ll.demo.member.entity.VerificationCode;
 import com.ll.demo.member.repository.EmailRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
 
-    // 인증 코드 생성 및 저장
-    public VerificationCode createVerificationCode(String email) {
-        VerificationCode code = VerificationCode.builder()
-                .email(email)
-                .code(UUID.randomUUID().toString()) // 랜덤 코드 생성
-                .expiresTime(LocalDateTime.now().plusDays(1)) // 1일 후 만료
-                .build();
-
-        return emailRepository.save(code);
+    public void sendEmail(String toEmail,String title, String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        System.out.println(emailForm);
+        try {
+            emailSender.send(emailForm);
+        } catch (RuntimeException e) {
+            e.printStackTrace(); // 또는 로거를 사용하여 상세한 예외 정보 로깅
+            throw new RuntimeException("Unable to send email in sendEmail", e); // 원인 예외를 포함시키기
+        }
     }
 
-    // 인증 코드 유효성 검사
-    public boolean verifyCode(String email, String code) {
-        return emailRepository.findByEmailAndCode(email, code)
-                .map(vc -> vc.getExpiresTime().isAfter(LocalDateTime.now()))
-                .orElse(false);
+    public SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+        return message;
     }
 
-    // 만료된 인증 코드 삭제 (스케줄링 작업 필요)
-    public void deleteExpiredVerificationCodes() {
-        emailRepository.deleteByExpiresTimeBefore(LocalDateTime.now());
-    }
+
 }
