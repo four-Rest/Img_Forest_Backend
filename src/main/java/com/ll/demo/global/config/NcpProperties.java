76ff08c00bb92369d075da7d371a7b7b4b3caf88
Bang210 <bangvelope@gmail.com>
2024-03-07 14:31:35 +0900

    feat: ncpProperties

---
+++ src/main/java/com/ll/demo/global/config/NcpProperties.java
@@ -0,0 +1,37 @@
+package com.ll.demo.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@Lazy
+@ConfigurationProperties(prefix = "spring.cloud.aws")
+public class NcpProperties {
+
+    private String accessKey;
+    private String secretKey;
+    private String regionName;
+    private String endpoint;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, regionName))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
