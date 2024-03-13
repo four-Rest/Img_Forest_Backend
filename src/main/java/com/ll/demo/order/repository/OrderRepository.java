d4986f3b8c6f9c4167ccd8eae0265c16f669d6f5
tallguy188 <cksghd4220@naver.com>
2024-03-13 19:15:45 +0900

    refactor: Order관련 코드 리팩토링
    order 관련 코드 리팩토링

--- src/main/java/com/ll/demo/order/repository/OrderRepository.java
+++ src/main/java/com/ll/demo/order/repository/OrderRepository.java
@@ -2,7 +2,23 @@ package com.ll.demo.order.repository;
 
 import com.ll.demo.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o from Order o" +
+            " left join fetch o.payment p" +
+            " left join fetch o.member m" +
+            " where o.orderUid = :orderUid")
+    Optional<Order> findOrderAndPaymentAndMember(String orderUid);
+
+    @Query("select o from Order o" +
+            " left join fetch o.payment p" +
+            " where o.orderUid = :orderUid")
+    Optional<Order> findOrderAndPayment(String orderUid);
 
 }
--- src/main/java/com/ll/demo/order/service/OrderService.java
+++ src/main/java/com/ll/demo/order/service/OrderService.java
@@ -3,12 +3,19 @@ package com.ll.demo.order.service;
 
 import com.ll.demo.article.entity.Article;
 import com.ll.demo.article.repository.ArticleRepository;
+import com.ll.demo.member.entity.Member;
 import com.ll.demo.order.dto.OrderRequestDto;
 import com.ll.demo.order.entity.Order;
 import com.ll.demo.order.repository.OrderRepository;
+import com.ll.demo.payment.entity.Payment;
+import com.ll.demo.payment.enums.PayStatus;
+import com.ll.demo.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.UUID;
 
 
 @Service
@@ -18,6 +25,30 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final ArticleRepository articleRepository;
+    private final PaymentRepository paymentRepository;
+
+    public Order saveOrderAndPayment(Member member,Article article) {
+
+        // 임시 결제내역 생성
+        Payment payment = Payment.builder()
+                .price(1000L)
+                .status(PayStatus.WAITING_FOR_PAYMENT)
+                .build();
+
+        paymentRepository.save(payment);
+
+        // 주문 생성
+        Order order = Order.builder()
+                .member(member)
+                .price(1000L)
+                .article(article)
+                .orderUid(UUID.randomUUID().toString())
+                .payment(payment)
+                .orderDate(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        return orderRepository.save(order);
+    }
 
 
 }
