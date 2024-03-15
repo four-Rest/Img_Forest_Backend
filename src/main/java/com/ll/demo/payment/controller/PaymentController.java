83a677f161f2fb41c09c8c7f7c9b21b7b92aaca0
tallguy188 <cksghd4220@naver.com>
2024-03-15 16:46:58 +0900

    feat: paymentByCallback 서비스 로직 추가
    서비스 로직 추가

--- src/main/java/com/ll/demo/payment/controller/PaymentController.java
+++ src/main/java/com/ll/demo/payment/controller/PaymentController.java
@@ -1,97 +1,97 @@
-//package com.ll.demo.payment.controller;
-//
-//
-//import com.ll.demo.global.response.GlobalResponse;
-//import com.ll.demo.member.entity.Member;
-//import com.ll.demo.member.service.MemberService;
-//import com.ll.demo.order.dto.OrderRequestDto;
-//import com.ll.demo.payment.dto.PaymentDto;
-//import com.ll.demo.payment.service.PaymentService;
-//import com.siot.IamportRestClient.IamportClient;
-//import com.siot.IamportRestClient.exception.IamportResponseException;
-//import com.siot.IamportRestClient.response.IamportResponse;
-//import com.siot.IamportRestClient.response.Payment;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.media.Content;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import jakarta.annotation.PostConstruct;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.io.IOException;
-//import java.security.Principal;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/payment")
-//@Slf4j
-//@Tag(name = "Payment", description = "Payment API")
-//@ApiResponses(value = {
-//        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
-//        @ApiResponse(responseCode = "400", description = "요청에 실패했습니다.", content = @Content(mediaType = "application/json")),
-//        @ApiResponse(responseCode = "401", description = "인증이 필요합니다.", content = @Content(mediaType = "application/json")),
-//        @ApiResponse(responseCode = "403", description = "요청이 거부되었습니다.", content = @Content(mediaType = "application/json")),
-//        @ApiResponse(responseCode = "404", description = "리소스를 서버에서 찾을 수 없습니다.", content = @Content(mediaType = "application/json"))
-//})
-//public class PaymentController {
-//
-//    private final PaymentService paymentService;
-//
-//    private IamportClient iamportClient;
-//
-//    @Value("${iamport.api.key}")
-//    private String restApiKey;
-//
-//    @Value("${iamport.api.secret}")
-//    private String restApiSecret;
-//
-//    @PostConstruct
-//    public void init() {
-//        this.iamportClient = new IamportClient(restApiKey,restApiSecret);
-//    }
-//
-//    // 결제 요청
+package com.ll.demo.payment.controller;
+
+
+import com.ll.demo.global.response.GlobalResponse;
+import com.ll.demo.member.entity.Member;
+import com.ll.demo.member.service.MemberService;
+import com.ll.demo.order.dto.OrderRequestDto;
+import com.ll.demo.payment.dto.PaymentDto;
+import com.ll.demo.payment.service.PaymentService;
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/payment")
+@Slf4j
+@Tag(name = "Payment", description = "Payment API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "요청에 실패했습니다.", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "401", description = "인증이 필요합니다.", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "403", description = "요청이 거부되었습니다.", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "리소스를 서버에서 찾을 수 없습니다.", content = @Content(mediaType = "application/json"))
+})
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    private IamportClient iamportClient;
+
+    @Value("${iamport.api.key}")
+    private String restApiKey;
+
+    @Value("${iamport.api.secret}")
+    private String restApiSecret;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(restApiKey,restApiSecret);
+    }
+
+    // 결제 요청
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("")
+    public GlobalResponse<IamportResponse<Payment>> validationPayment(@RequestBody PaymentDto paymentDto) {
+        IamportResponse<Payment> iamportResponse =  paymentService.paymentByCallback(paymentDto);
+        log.info("결제 응답={}", iamportResponse.getResponse().toString());
+
+        return GlobalResponse.of("200","결제요청완료",iamportResponse);
+
+    }
+
+
+//    // 결제 성공
 //    @PreAuthorize("isAuthenticated()")
-//    @PostMapping("")
-//    public GlobalResponse<IamportResponse<Payment>> validationPayment(@RequestBody PaymentDto paymentDto) {
-//        IamportResponse<Payment> iamportResponse =  paymentService.paymentByCallback(paymentDto);
-//        log.info("결제 응답={}", iamportResponse.getResponse().toString());
+//    @PostMapping("/payment/validate")
+//    @Operation(summary = "결제완료 및 결제정보저장", description = "결제완료 및 결제정보저장 시 사용하는 API")
+//    public GlobalResponse createPayment(Principal principal, @RequestBody PaymentReq paymentReq) throws IOException {
+//        String orderNumber = String.valueOf(paymentReq.getOrderNumber());
+//        String username = principal.getName();
+//        Member member = memberService.findByUsername(username);
 //
-//        return GlobalResponse.of("200","결제요청완료",iamportResponse);
-//
+//        paymentService.saveOrder(member, paymentReq);
+//        log.info("결제 성공: 주문번호 {}", orderNumber);
+//        return GlobalResponse.of("200", "결제 성공");
 //    }
 //
 //
-////    // 결제 성공
-////    @PreAuthorize("isAuthenticated()")
-////    @PostMapping("/payment/validate")
-////    @Operation(summary = "결제완료 및 결제정보저장", description = "결제완료 및 결제정보저장 시 사용하는 API")
-////    public GlobalResponse createPayment(Principal principal, @RequestBody PaymentReq paymentReq) throws IOException {
-////        String orderNumber = String.valueOf(paymentReq.getOrderNumber());
-////        String username = principal.getName();
-////        Member member = memberService.findByUsername(username);
-////
-////        paymentService.saveOrder(member, paymentReq);
-////        log.info("결제 성공: 주문번호 {}", orderNumber);
-////        return GlobalResponse.of("200", "결제 성공");
-////    }
-////
-////
-////    // 결제 취소
-////    @PreAuthorize("isAuthenticated()")
-////    @PostMapping("/payment/cancel")
-////    @Operation(summary = "결제취소", description = "결제취소 시 사용하는 API")
-////    public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) throws IamportResponseException, IOException {
-////        IamportResponse<Payment> payment = iamportClient.paymentByImpUid(imp_uid);
-////        log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}",payment.getResponse().getMerchantUid());
-////        return payment;
-////    }
-//
-//
-//
-//}
+//    // 결제 취소
+//    @PreAuthorize("isAuthenticated()")
+//    @PostMapping("/payment/cancel")
+//    @Operation(summary = "결제취소", description = "결제취소 시 사용하는 API")
+//    public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) throws IamportResponseException, IOException {
+//        IamportResponse<Payment> payment = iamportClient.paymentByImpUid(imp_uid);
+//        log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}",payment.getResponse().getMerchantUid());
+//        return payment;
+//    }
+
+
+
+}
--- src/main/java/com/ll/demo/payment/service/PaymentService.java
+++ src/main/java/com/ll/demo/payment/service/PaymentService.java
@@ -1,22 +1,41 @@
 package com.ll.demo.payment.service;
 
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.ll.demo.article.entity.Article;
 import com.ll.demo.article.repository.ArticleRepository;
 import com.ll.demo.member.entity.Member;
+import com.ll.demo.member.service.MemberService;
 import com.ll.demo.order.dto.OrderRequestDto;
 import com.ll.demo.order.entity.Order;
 import com.ll.demo.order.repository.OrderRepository;
+import com.ll.demo.order.service.OrderService;
 import com.ll.demo.payment.dto.PaymentDto;
+import com.ll.demo.payment.enums.PayStatus;
+import com.ll.demo.payment.repository.PaymentRepository;
 import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import jakarta.annotation.PostConstruct;
+import lombok.Builder;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 
 @Service
@@ -25,10 +44,50 @@ import java.sql.Timestamp;
 public class PaymentService {
     private final OrderRepository orderRepository;
     private final ArticleRepository articleRepository;
+    private final PaymentRepository paymentRepository;
 
+    private final IamportClient iamportClient;
 
-//    public IamportResponse<Payment> paymentByCallback(PaymentDto paymentDto) {
-//
-//    }
+    public IamportResponse<Payment> paymentByCallback(PaymentDto paymentDto) {
 
+        try {
+            // 결제 단건 조회
+            IamportResponse<Payment> iamportResponse = iamportClient.paymentByImpUid(paymentDto.getImpUid());
+            // 주문 내역 조회
+            Order order = orderRepository.findOrderAndPayment(paymentDto.getOrderUid())
+                    .orElseThrow(() -> new IllegalArgumentException("주문 내역이 존재하지 않습니다"));
+
+            // 결제 완료가 아니면
+            if(!iamportResponse.getResponse().getStatus().equals("paid")) {
+                // 주문, 결제 삭제
+                orderRepository.delete(order);
+                paymentRepository.delete(order.getPayment());
+                throw new RuntimeException("결제 미완료");
+            }
+            Long price = order.getPayment().getPrice();
+            int iamportPrice = iamportResponse.getResponse().getAmount().intValue();
+            // 결제 금액 검증
+            if(iamportPrice != price) {
+                // 결제 금액이 다르면 주문, 결제 삭제
+                orderRepository.delete(order);
+                paymentRepository.delete(order.getPayment());
+                // 결제금액 위변조로 의심되는 결제금액을 취소(아임포트)
+                iamportClient.cancelPaymentByImpUid(new CancelData(iamportResponse.getResponse().getImpUid(), true, new BigDecimal(iamportPrice)));
+
+                throw new RuntimeException("결제금액 위변조 의심");
+            }
+
+            // 결제 상태 변경
+            order.getPayment().changePaymentBySuccess(PayStatus.SUCCESS, iamportResponse.getResponse().getImpUid());
+
+            return iamportResponse;
+
+        } catch (IamportResponseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
